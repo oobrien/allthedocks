45a0b45c454eb9d2d4a06cfb47103fc31c6483e2
Oliver O'Brien <oobrien@users.noreply.github.com>
2023-07-10 11:44:17 +0100

    Website updates for ATD2 - 5 teams, scoreboard, enhanced API
    Plus use updated version of OpenLayers.

--- atdmap/live.js
+++ atdmap/live.js
@@ -7,39 +7,74 @@ function initComplete()
 		var w = [];
 		var s = [];
 		var e = [];
+		var p = [];
+		var n = [];
 
 		for (var i in docks)
 		{
 			var team = (docks[i][5][0]);
+			var html = "<div><div class='cb'><label><input class='" + team + "' type='checkbox' id='cb" + docks[i][0] + "' onchange='saveedit(" + docks[i][0] + ", \"" + team + "\")' />" + docks[i][4] + "</label>&nbsp;<span id='vt" + docks[i][0] +  "'></span></div></div>";
 			if (team == "W") 
 			{ 
-					w.push("<div><div class='cb'><label><input type='checkbox' id='cb" + docks[i][0] + "' onchange='saveedit(" + docks[i][0] + ")' />" + docks[i][4] + "</label>&nbsp;<span id='vt" + docks[i][0] +  "'></span></div></div>");
+					w.push(html);
 			}
 			if (team == "S") 
 			{ 
-					s.push("<div><div class='cb'><label><input type='checkbox' id='cb" + docks[i][0] + "' onchange='saveedit(" + docks[i][0] + ")' />" + docks[i][4] + "</label>&nbsp;<span id='vt" + docks[i][0] +  "'></span></div></div>");
+					s.push(html);
 			}
 			if (team == "E") 
 			{ 
-					e.push("<div><div class='cb'><label><input type='checkbox' id='cb" + docks[i][0] + "' onchange='saveedit(" + docks[i][0] + ")' />" + docks[i][4] + "</label>&nbsp;<span id='vt" + docks[i][0] +  "'></span></div></div>");
+					e.push(html);
+			}
+			if (team == "P") 
+			{ 
+					p.push(html);
+			}
+			if (team == "N") 
+			{ 
+					n.push(html);
 			}
 		}	
  		document.getElementById('westlist').innerHTML = w.join("");
 		document.getElementById('southlist').innerHTML = s.join("");
 		document.getElementById('eastlist').innerHTML = e.join("");	 	
+		document.getElementById('putneylist').innerHTML = p.join("");	 	
+		document.getElementById('northlist').innerHTML = n.join("");	 	
 		getVisited();
 
 	}})		
 	setInterval(getVisited, 1000*60*1);
+	
+	var hash = window.location.hash.substring(1);
+
+	if (hash != '')
+	{
+		document.getElementById('segmentchooser').value = hash;
+		filter();
+	}
 }
 
-function saveedit(tfl_id)
+var lockedotherteams = false;
+function saveedit(tfl_id, team)
 {
 	var element = $('#cb' + tfl_id);
 	$('#vt' + tfl_id).html('');
 	
+	if (!lockedotherteams)
+	{
+		$('input[type=checkbox]').each(function() {
+		   if ($(this).attr("class") != team) {
+				$(this).prop("disabled", true);
+		   }
+		});	
+		lockedotherteams = true;	
+	}
+
 	var checked = $('#cb' + tfl_id).prop('checked');
-	
+	if (checked)
+	{
+			$('#vt' + tfl_id).html('*****');
+	}	
 	$.ajax({
 		url: 'saveedit.php',
 		method: 'POST',
@@ -53,3 +88,8 @@ function saveedit(tfl_id)
 		}
 	}); 
 }
+
+$(document).ready(function()
+{
+	initComplete();
+});
--- atdmap/live.php
+++ atdmap/live.php
@@ -5,23 +5,34 @@
  	<title>All the Docks LIVE</title>
 
 	<link rel="stylesheet" type="text/css" media="screen" href="style.css" />	
-	<link rel="stylesheet" type="text/css" media="all" href="https://lib.oomap.co.uk/openlayers/v6.15.1-dist/ol.css" />	
+	<link rel="stylesheet" type="text/css" media="all" href="https://lib.oomap.co.uk/openlayers/v7.4.0-dist/ol.css" />	
 
  	<script type="text/javascript" src="https://lib.oomap.co.uk/jquery/jquery-3.5.1.js"></script>	     
-    <script type="text/javascript" src="https://lib.oomap.co.uk/openlayers/v6.15.1-dist/ol.js"></script> 
+    <script type="text/javascript" src="https://lib.oomap.co.uk/openlayers/v7.4.0-dist/ol.js"></script> 
 	<script type="text/javascript" src="main.js"></script>
 	<script type="text/javascript" src="live.js"></script>
 </head>
-<body onload="init()">
+<body>
     <div id="mapcontainer" style='bottom: 200px;'></div>
-    <select id="segmentchooser" onchange="filter()"><option id="all">All</option></select>
-    <div id='link'><a href='diff.php'>Late changes</a></div>
+    <select id="segmentchooser" onchange="filter()"><option id="all">All Stages</option></select>
+    <div id="scoreboard">
+    	<table><tr>
+    	<th id='sbtW'>W</th><td id='sbW'>...</td>
+    	<th id='sbtP'>P</th><td id='sbP'>...</td>
+    	<th id='sbtS'>S</th><td id='sbS'>...</td>
+    	<th id='sbtN'>N</th><td id='sbN'>...</td>
+    	<th id='sbtE'>E</th><td id='sbE'>...</td>
+    	</tr></table>
+    </div>
+    <div id='link'><span>Fot teams use only.</span> </span> <a href='/'>Public map</a> - <a href='https://github.com/oobrien/allthedocks'>GPX files</a> - <a href='diff.php'>Late changes</a></div>
 	<div id="list">
 	 <table>
-		<tr><th>WEST</th><th>SOUTH</th><th>EAST</th></tr>
+		<tr id='ltrow'><th id='ltW'>WEST</th><th id='ltP'>PUTNEY</th><th id='ltS'>SOUTH</th><th id='ltN'>NORTH</th><th id='ltE'>EAST</th></tr>
 		<tr>
 		<td id='westlist'></td>
+		<td id='putneylist'></td>
 		<td id='southlist'></td>
+		<td id='northlist'></td>
 		<td id='eastlist'></td>
 		</tr>
 		</table>	
